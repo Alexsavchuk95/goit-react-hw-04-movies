21d10b00ff5adbef27071f2ea6e33460c106ba7c
Alexsavchuk95 <savchukalex57@gmail.com>
2019-12-17 21:11:35 +0300

    Updates

---
+++ precache-manifest.7da81dc7732749c97284abde3551c84f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f00c379747aa81311eaefa5cdc58c53",
+    "url": "/goit-react-hw-04-movies/index.html"
+  },
+  {
+    "revision": "1b6e392aa22987aecfbe",
+    "url": "/goit-react-hw-04-movies/static/css/main.71239964.chunk.css"
+  },
+  {
+    "revision": "435254da45314c635860",
+    "url": "/goit-react-hw-04-movies/static/js/2.eba171bb.chunk.js"
+  },
+  {
+    "revision": "1b6e392aa22987aecfbe",
+    "url": "/goit-react-hw-04-movies/static/js/main.7f69421e.chunk.js"
+  },
+  {
+    "revision": "74e0430584fef6c1a1c3",
+    "url": "/goit-react-hw-04-movies/static/js/runtime-main.ca55bea9.js"
+  }
+]);
